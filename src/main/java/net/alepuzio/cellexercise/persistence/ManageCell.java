189a24a8cfd9eea43bac9276a81c30585fcdc951
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2023-11-29 11:27:10 +0100

    fix wrong import

--- src/main/java/net/alepuzio/cellexercise/persistence/ManageCell.java
+++ src/main/java/net/alepuzio/cellexercise/persistence/ManageCell.java
@@ -1,26 +1,12 @@
 package net.alepuzio.cellexercise.persistence;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import net.alepuzio.cellexercise.functional.position.Longitude;
-import net.alepuzio.cellexercise.functional.position.Point;
-import net.alepuzio.cellexercise.persistence.entity.Cell;
-import net.alepuzio.cellexercise.persistence.repository.CellEventRepository;
 
 @Component
 public class ManageCell {
 
 
-		
-		@Autowired
-		private CellEventRepository cellEventRepository;
-
+	
 		@Autowired
 		private CellRepository cellRepository;
 
@@ -28,9 +14,7 @@ public class ManageCell {
 			return  cellRepository.findAll();
 		}
 		
-		public Iterable<CellEvent> readAllCellEvent() throws Exception {
-			return cellEventRepository.findAll();
-		}
+	
 			
 
 }
